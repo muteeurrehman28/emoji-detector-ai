d00be6d0914233b8e39577f388d1039ca3a4f7db
aurangzebmalik <bsai23021@itu.edu.pk>
2025-05-18 22:01:13 +0500

    Updated preparing_dataset.py with making folders in directory

--- scripts/preparing_dataset.py
+++ scripts/preparing_dataset.py
@@ -12,3 +12,15 @@ with rarfile.RarFile(rar_path) as rf:
 print("✅ Unzipped successfully!")
 
 len(os.listdir("/content/drive/MyDrive/emoji_classify/Category_Wise_Aug"))
+
+
+
+# Set the base directory where your images and label.txt are located
+base_folder = "/content/drive/MyDrive/emoji_classify"  # <- change this if needed
+
+# Input folder (where cropped transparent emojis are stored)
+input_folder = os.path.join(base_folder, "Category Wise Emojis")  # assuming subfolders exist
+
+# Output folder for augmented images
+output_folder = os.path.join(base_folder, "Cropped_Augmented")
+os.makedirs(output_folder, exist_ok=True)
